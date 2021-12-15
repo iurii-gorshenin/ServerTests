f7b7976dcfb5567e169301753c5cd632099239a9
Vitalii_Lozhnikov <Vitalii_Lozhnikov@epam.ru>
2021-12-15 16:46:54 +0300

    add new projects

---
+++ ClientApp - auto/Client/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static int port = 8888; 
+        static string address = "127.0.0.1";
+        static string fileName = Guid.NewGuid().ToString() + ".txt";
+        static List<string> names = new List<string>() { "Glen", "Max", "Tim", "John", "Andrew" };
+        static List<string> answers = new List<string>() 
+        { 
+            "Hello!", "How are you?", "What's it?", "Fine, thank you!", "Glad to see you.", "I am ok and you?", "Oh, it is a good place",
+            "Let's play!", "Muhahaha", "ok"
+        };
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                bool sendName = false;
+                bool needChat = true;
+                try
+                {
+                    IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+
+                    Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    socket.Connect(ipPoint);
+
+                    //реализация для треда, а не tpl
+                    //var writerThread = new Thread(writerActions);
+                    //writerThread.Start(socket);
+                    Task.Run(()=>writerActions(socket));
+
+                    int times = rnd.Next(1, 5);
+
+                    while (SocketConnected(socket) && needChat)
+                    {
+                        string message = "";
+                        if (sendName)
+                        {
+                            message = answers[rnd.Next(0, answers.Count)];
+                            Thread.Sleep(rnd.Next(0, 5000));
+                        }
+                        else
+                        {
+                            message = names[rnd.Next(0, names.Count)];
+                            sendName = true;
+                        }
+                        if (SocketConnected(socket) && message!="")
+                        {
+                            byte[] data = Encoding.Unicode.GetBytes(message);
+                            socket.Send(data);
+                        }
+                        if (times < 1)
+                        {
+                            needChat = false;
+                            Console.WriteLine("Client ended his task");
+                        }
+                        times = times - 1;
+                    }   
+
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                if (needChat)
+                {
+                    Console.WriteLine("Server closed all connections...");
+                    break;
+                }
+            }
+            Console.Read();
+        }
+        static void writerActions(object obj)
+        {
+            fileName = Guid.NewGuid().ToString() + ".txt";
+            var socket = (Socket)obj;
+            while(SocketConnected(socket))
+            {
+                try
+                {
+                    byte[] data = new byte[256];
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+
+                    do
+                    {
+                        bytes = socket.Receive(data, data.Length, 0);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (socket.Available > 0);
+
+                   
+                    using (StreamWriter sw = File.AppendText(Environment.CurrentDirectory +"\\"+ fileName))
+                    {
+                        sw.WriteLine(builder.ToString());
+                    }
+                }
+                catch
+                {
+                    break;
+                }
+            }
+
+        }
+        static bool SocketConnected(Socket s)
+        {
+            try
+            {
+                bool part1 = s.Poll(1000, SelectMode.SelectRead);
+                bool part2 = (s.Available == 0);
+                if (part1 && part2)
+                    return false;
+                else
+                    return true;
+            }
+            catch
+            {
+                return false;
+            }           
+        }
+    }
+}
---
+++ Server/ConsoleApp1/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static int port = 8888;
+        static string ipAddress = "127.0.0.1"; 
+        static List<Socket> clients = new List<Socket>();
+        static List<string> chatMsg = new List<string>();
+        [DllImport("Kernel32")]
+        private static extern bool SetConsoleCtrlHandler(EventHandler handler, bool add);
+        private delegate bool EventHandler();
+        static EventHandler _handler;
+
+        static void Main(string[] args)
+        {
+            _handler += new EventHandler(Handler);
+            SetConsoleCtrlHandler(_handler, true);
+    
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(ipAddress), port);
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                listenSocket.Bind(ipPoint);
+
+                //слушаем...
+                listenSocket.Listen(100);
+                Console.WriteLine("Waiting for clients...");
+
+                int count = 0;
+                while (true)
+                {
+                    Socket handler = listenSocket.Accept();
+                    Console.WriteLine("client "+(count+=1)+" entered chatroom (one chat session statistics)");
+
+                    var connectionData = new ConnectionData(handler, clients, chatMsg);
+                    //вариант с созданием треда, а не tpl
+                    //Thread userCtrl = new Thread(ClientThread);
+                    //userCtrl.Start( );
+
+                    Task.Run(()=> ClientThread(connectionData));
+                    clients.Add(handler);
+                    
+                    string history = "";
+                    if (chatMsg.Count != 0)
+                    {
+                        foreach (string str in chatMsg)
+                        {
+                            history += str + Environment.NewLine;
+                        }                       
+                        handler.Send(Encoding.Unicode.GetBytes(history));
+                    }                        
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static void ClientThread(object StateInfo)
+        {
+            var connectionData = (ConnectionData)StateInfo;
+            var handler = connectionData.Handler;
+            int bytes = 0;
+            byte[] data = new byte[256];
+            string userName = "";
+            while (SocketConnected(handler))
+            {
+                StringBuilder builder = new StringBuilder();
+                try
+                {                 
+                    do
+                    {
+                        bytes = handler.Receive(data);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (handler.Available > 0);
+                    if(builder.ToString()!="")
+                    {
+                        string chatMessage = "";
+                        if (userName == "")
+                        {
+                            userName = builder.ToString();
+                            chatMessage = DateTime.Now.ToShortTimeString() + " " + userName + " connected";
+                            Console.WriteLine(DateTime.Now.ToShortTimeString() + " " + userName + " connected");
+                        }
+                        else
+                        {
+                            chatMessage = DateTime.Now.ToShortTimeString() + " " + userName + ": " + builder.ToString();
+                            Console.WriteLine(chatMessage);
+                        }
+
+                        Task.Run(() =>
+                        {
+                            foreach (var client in connectionData.Clients)
+                            {
+                                client.Send(Encoding.Unicode.GetBytes(chatMessage));
+                            }
+                        });
+                        
+                        lock (ConnectionData.lockerObj)
+                        {
+                            chatMsg.Add(chatMessage);
+                        }
+                    }                                     
+                }
+                catch
+                {
+                    break;
+                }
+
+            }
+            Console.WriteLine("client "+ userName + " was disconnected");
+            lock (ConnectionData.lockerObjCli)
+            {
+                connectionData.Clients.Remove(handler);
+            }
+            handler.Shutdown(SocketShutdown.Both);
+            handler.Close();
+        }
+        static bool SocketConnected(Socket s)
+        {
+            bool part1 = s.Poll(1000, SelectMode.SelectRead);
+            bool part2 = (s.Available == 0);
+            if (part1 && part2)
+                return false;
+            else
+                return true;
+        }
+        
+
+        private static bool Handler()
+        {
+            Stop();
+            return false;
+        }
+        public static void Stop()
+        {
+            foreach(var cli in clients)
+            {
+                cli.Send(Encoding.Unicode.GetBytes("Server close all connections, goodbye!"));
+                cli.Shutdown(SocketShutdown.Both);
+                cli.Close();
+            }           
+        }
+    }
+
+    class ConnectionData
+    {
+        public Socket Handler { get; set; }
+        public List<Socket> Clients { get; set; }
+        public List<string> ChatMsg { get; set; }
+        public static object lockerObj = new object();
+        public static object lockerObjCli = new object();
+        public ConnectionData(Socket socket, List<Socket> sockets, List<string> chatMsg)
+        {
+            Handler = socket;
+            Clients = sockets;
+            ChatMsg = chatMsg;
+        }
+    }
+}
